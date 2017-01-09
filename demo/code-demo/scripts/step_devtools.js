c967b564be7087dc09f87fe61e5f48537b63ddd5
Najib Abu Samra <t-naabus@microsoft.com>
2017-01-09 15:57:49 +0200

    Fixed URL

--- demo/code-demo/scripts/step_devtools.js
+++ demo/code-demo/scripts/step_devtools.js
@@ -13,13 +13,13 @@ function OpenEmbedStepWithDevtoolSample(sampleType) {
     var staticReportUrl = null;
     switch (sampleType){
         case 'MSIT':
-            staticReportUrl = 'http://localhost:1248/api/reports?id=bf33002e-9adc-452d-a0b5-fb649d806358&env=Msit';
+            staticReportUrl = 'https://powerbi-embed-api.azurewebsites.net/api/reports?id=bf33002e-9adc-452d-a0b5-fb649d806358&env=Msit';
             break;
         case 'DXT':
-            staticReportUrl = 'http://localhost:1248/api/reports?id=e54c36f0-46eb-4d17-8121-f98e5148c8be&env=Dxt';
+            staticReportUrl = 'https://powerbi-embed-api.azurewebsites.net/api/reports?id=e54c36f0-46eb-4d17-8121-f98e5148c8be&env=Dxt';
             break;
         case 'EDOG':
-            staticReportUrl = 'http://localhost:1248/api/reports?id=f360ea1a-3d5c-4aca-a3e5-303f30c84c05&env=Edog';
+            staticReportUrl = 'https://powerbi-embed-api.azurewebsites.net/api/reports?id=f360ea1a-3d5c-4aca-a3e5-303f30c84c05&env=Edog';
             break;
     }
 
